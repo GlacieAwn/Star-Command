09f3ce3d1a8d3fdfaf2c6e85bf8530e89e8422ba
glacieawn <squeakyfoxx@proton.me>
2026-02-25 23:56:33 -0500

    Implement load_scene function to manage scene loading and visibility

--- Scripts/scene_manager.gd
+++ Scripts/scene_manager.gd
@@ -2,4 +2,18 @@ extends Node
 
 var scene_instance: Node
 
-#TODO: Create a load_scene function with 2 arguments: scene name as path, and if the scene should be queue_free()'d or just hidden(useful for gameplay vs menus), and returns that scene to be added as a child of other nodes for flexibility
+# Loads a scene with the given path, and decide whether to hide or delete the current scene depending on the input of `free`. 
+# If free is true, then the scene is deleted, only getting loaded again when called. If false, then the scene is merely hidden. This is useful for menus, where the current scene might not need to be completely destroyed as you might need to go back to it. 
+func load_scene(path: String, free: bool) -> Node:
+	if scene_instance:
+		if free:
+			scene_instance.queue_free()
+		else:
+			scene_instance.hide()
+
+	var loaded_scene = ResourceLoader.load(path)
+	if loaded_scene == null:
+		return
+	
+	scene_instance = loaded_scene.instantance()
+	return scene_instance
