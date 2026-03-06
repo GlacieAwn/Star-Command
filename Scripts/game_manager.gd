6f5615b90b090f75c0b726ea319802cdd78ce263
Glacie <168385216+GlacieAwn@users.noreply.github.com>
2026-03-06 15:25:27 -0500

    Refactor screen transition effects: remove old ScreenTransition and implement Screen Fade with animations

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -1,22 +1,33 @@
 extends Node
 
+var splash_done: bool = false
 var title_scene_loaded: bool = false
 var gameplay_loaded: bool = false
 var cur = 0
 
 func _ready() -> void:
 	Global.game_manager = self	
+
+	$"Screen Fade/AnimationPlayer".play("RESET")
+
+	# handle splash screens
 	
-	if not title_scene_loaded:
+	if splash_done and not title_scene_loaded:
+		$"Screen Fade/AnimationPlayer".play_backwards("Fade")
 		Global.audio_manager.play_music(Global.title_theme_dmg)
 		var title_scene_instance = Global.scene_manager.load_scene("res://Scenes/Menus/Title.tscn", false)
 		$SubViewport.add_child(title_scene_instance)
 		title_scene_loaded = true
 
+
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("Start") and not gameplay_loaded:
-		Global.audio_manager.play_music(Global.gameplay_theme_dmg)
+		Global.audio_manager.stop_music()
+		$"Screen Fade/AnimationPlayer".play("Fade")
+		await $"Screen Fade/AnimationPlayer".animation_finished
+		$"Screen Fade/AnimationPlayer".play_backwards("Fade")
 		var gameplay_scene_instance = Global.scene_manager.load_scene("res://Scenes/Gameplay.tscn", true)
+		Global.audio_manager.play_music(Global.gameplay_theme_dmg)
 		$SubViewport.add_child(gameplay_scene_instance)
 		gameplay_loaded = true
 	
