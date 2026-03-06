f2ae5c1ed22286dced757a5d1d10eb9d422d77dc
Glacie <168385216+GlacieAwn@users.noreply.github.com>
2026-03-06 01:04:00 -0500

    Remove Gameplay scene reference and update game_manager for scene loading logic

--- Scripts/game_manager.gd
+++ Scripts/game_manager.gd
@@ -2,33 +2,32 @@ extends Node
 
 # var gameplay_theme_modern = preload("res://Audio/Modern/Let's Go!.ogg")
 
-var game_state: Global.GAME_STATE
+var title_scene_loaded: bool = false
+var gameplay_loaded: bool = false
 var cur = 0
 
 func _ready() -> void:
 	Global.game_manager = self	
-	game_state = Global.GAME_STATE.GAMEPLAY
-
-	match game_state:
-		Global.GAME_STATE.TITLE:
-			Global.audio_manager.play_music(Global.title_theme_dmg)
-			pass
-		
-		Global.GAME_STATE.GAMEPLAY:
-			Global.audio_manager.play_music(Global.gameplay_theme_dmg)
-			pass
 	
-func _process(_delta: float) -> void: 
-	match game_state:
-		Global.GAME_STATE.TITLE:
-			var title_scene_instance = Global.scene_manager.load_scene("res://Scenes/Menus/Title.tscn", false)
-			$SubViewport.add_child(title_scene_instance)
+	# Load title scene
+	if not title_scene_loaded:
+		Global.audio_manager.play_music(Global.title_theme_dmg)
+		var title_scene_instance = Global.scene_manager.load_scene("res://Scenes/Menus/Title.tscn", false)
+		$SubViewport.add_child(title_scene_instance)
+		title_scene_loaded = true
 
-		Global.GAME_STATE.GAMEPLAY:
-			$UI/ScoreText.text = str(Global.score)
-			$UI/LivesText.text = str("*", Global.player.lives)
-		_:
-			pass
+func _process(_delta: float) -> void:
+	# Check for start input to load gameplay
+	if Input.is_action_just_pressed("Start") and not gameplay_loaded:
+		Global.audio_manager.play_music(Global.gameplay_theme_dmg)
+		var gameplay_scene_instance = Global.scene_manager.load_scene("res://Scenes/Gameplay.tscn", true)
+		$SubViewport.add_child(gameplay_scene_instance)
+		gameplay_loaded = true
+	
+	# Update UI during gameplay
+	if gameplay_loaded:
+		$UI/ScoreText.text = str(Global.score)
+		$UI/LivesText.text = str("*", Global.player.lives)
 	
 	#print(Global.score)
 	pass
@@ -40,10 +39,7 @@ func process(_delta: float) -> void:
 	pass
 	
 func clear_screen() -> void:
-
-	if game_state == Global.GAME_STATE.GAMEPLAY:
+	if gameplay_loaded:
 		for node in $SubViewport/Gameplay/EnemySpawner/Enemies.get_children():
 			node.queue_free()
-	
-	pass
 	
